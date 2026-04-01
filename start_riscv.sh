ba04d18d3535cdc3a94ebbf7a00f7ef52439b7ef
Dmitry Levkin <mit.levkin@vk.com>
2026-04-01 10:27:57 +0300

    made the script safe to use and add prefix into the chroot

--- start_riscv.sh
+++ start_riscv.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cleanup() {
-    echo "cleaning"
+    echo -e "\n[!] cleaning up mounts..."
+    sudo umount -l ~/riscv_vm/root/simd_blur 2>/dev/null
     sudo umount -l ~/riscv_vm/dev/pts 2>/dev/null
     sudo umount -l ~/riscv_vm/dev 2>/dev/null
     sudo umount -l ~/riscv_vm/sys 2>/dev/null
@@ -19,5 +20,7 @@ sudo mount --bind /dev/pts ~/riscv_vm/dev/pts
 sudo sysctl -w kernel.perf_event_paranoid=-1
 sudo sysctl -w kernel.kptr_restrict=0
 
+sudo mount --bind ~/Projects/python/simd_blur ~/riscv_vm/root/simd_blur
+
 echo "entering chroot"
-sudo chroot ~/riscv_vm /bin/bash
+sudo chroot ~/riscv_vm /bin/bash --login -c "cd /root/simd_blur && exec /bin/bash -i"
